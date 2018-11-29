b08c99836ba5490b6617cdb99ac95192cfd697b1
denis <deoniisii@gmail.com>
2018-11-29 16:48:22 +0200

    mace road

--- app/src/main/java/denisimus_it/net/navigator/MapsActivity.java
+++ app/src/main/java/denisimus_it/net/navigator/MapsActivity.java
@@ -3,22 +3,30 @@ package denisimus_it.net.navigator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    public static final String MY_LOG = "My_log";
 
     private GoogleMap mMap;
     private float startPointLat;
     private float startPointLng;
     private float endPointLat;
     private float endPointLng;
+    private List<LatLng> points = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             startPointLng = Float.valueOf(intent.getStringExtra("startPointLng"));
             endPointLat = Float.valueOf(intent.getStringExtra("endPointLat"));
             endPointLng = Float.valueOf(intent.getStringExtra("endPointLng"));
+            points = PolyUtil.decode(intent.getStringExtra("points"));
+
+            Log.d(MY_LOG, "points: " + points.toString());
         }
 
 
@@ -45,14 +56,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng startPoint = new LatLng(startPointLat, startPointLng);
-        LatLng endPoint = new LatLng(endPointLat, endPointLng);
+        PolylineOptions line = new PolylineOptions();
+        line.width(4f);
+        LatLngBounds.Builder latLngBuilder = new LatLngBounds.Builder();
 
-        mMap.addMarker(new MarkerOptions().position(startPoint).title("Start point"));
-        mMap.addMarker(new MarkerOptions().position(endPoint).title("End point"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(startPoint));
-        PolylineOptions line = new PolylineOptions().add();
-        mMap.addPolyline(line);
+        if (points != null) {
+            for (int i = 0; i < points.size(); i++) {
+                if (i == 0) {
+                    MarkerOptions startMarkerOptions = new MarkerOptions()
+                            .position(points.get(i));
+                    mMap.addMarker(startMarkerOptions);
+                } else if (i == points.size() - 1) {
+                    MarkerOptions endMarkerOptions = new MarkerOptions()
+                            .position(points.get(i));
+                    mMap.addMarker(endMarkerOptions);
+                }
+                line.add(points.get(i));
+                latLngBuilder.include(points.get(i));
+            }
+            mMap.addPolyline(line);
+            int size = getResources().getDisplayMetrics().widthPixels;
+            LatLngBounds latLngBounds = latLngBuilder.build();
+            CameraUpdate track = CameraUpdateFactory.newLatLngBounds(latLngBounds, size, size, 25);
+            mMap.moveCamera(track);
+        }
     }
 }
